81ee568b35d5462774edc5d4c77f26afd104caf6
Dilip <dilip7844@gmail.com>
2023-04-22 16:54:59 +0530

    init commit, Added Firebase Auth dependencies.

---
+++ app/src/main/java/com/example/test/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.test.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding:ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
